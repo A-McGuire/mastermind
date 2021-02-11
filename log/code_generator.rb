a448748e32a006307217231d1f85436f170ff86c
Gunnar Runkle <dunkcrunk@gmail.com>
2021-02-10 19:06:20 -0700

    Update code_generator, create test_Class

---
+++ log/code_generator.rb
@@ -0,0 +1,8 @@
+class CodeGenerator
+
+  def secret_code
+    colors = ["r", "r", "r", "r", "g", "g", "g", "g", "b", "b", "b", "b", "y", "y", "y", "y"]
+    @secret_code = colors.shuffle
+    @secret_code[1..4]
+  end
+end
