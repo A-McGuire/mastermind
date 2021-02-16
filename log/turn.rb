e2464c4cc53a506b2f2e37af1ea9ad0e5b6bf97a
Gunnar Runkle <dunkcrunk@gmail.com>
2021-02-16 13:37:42 -0700

    Refactor user input methods.

--- log/turn.rb
+++ log/turn.rb
@@ -12,33 +12,57 @@ class Turn
   end
 
   def user_input
-    input = gets.chomp
-    if input == "q" || input == "quit"
-      puts "You lose ;)"
-      line_break
-      exit
-    elsif input == "c" || input == "cheat"
-      p @secret_code.join.upcase
-      p "Cheater!"
-      line_break
-      user_input
-    elsif input.length == 4
-      @turn_input = input.split("")
-      @turn_input.each do |peg|
-        peg.downcase
-      end
-    elsif input.length > 4
-      p "Too many characters"
-      line_break
-      user_input
-    elsif input.length < 4
-      p "Not enough characters"
-      line_break
-      user_input
+    @input = gets.chomp
+    if @input == "q" || @input == "quit"
+      quit_game
+    elsif @input == "c" || @input == "cheat"
+      show_secret_code
+    elsif @input.length == 4
+      sanitize_input
+    elsif @input.length > 4
+      if_input_too_long
+    elsif @input.length < 4
+      if_input_too_short
     else
-      "Invalid Input"
-      line_break
-      user_input
+      invalid_input
     end
+  end
+
+  def quit_game
+    puts "You lose ;)"
+    line_break
+    exit
+  end
+
+  def show_secret_code
+    p @secret_code.join.upcase
+    p "Cheater!"
+    line_break
+    user_input
+  end
+
+  def sanitize_input
+    @turn_input = @input.split("")
+    @turn_input.each do |peg|
+      peg.downcase
+    end
+  end
+
+  def if_input_too_long
+    p "Too many characters"
+    line_break
+    user_input
+  end
+
+  def if_input_too_short
+    p "Not enough characters"
+    line_break
+    user_input
+  end
+
+  def invalid_input
+    "Invalid Input"
+    line_break
+    user_input
   end
 end
